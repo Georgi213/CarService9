78828eb960027145d09cf87fd2f52c719576b1e6
User <User@DESKTOP-E16DB6R>
2022-12-05 19:41:47 +0200

    Добавьте файлы проекта.

---
+++ WebApplication1/Models/CreateICS.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace StartFromScratch
+{
+    public class CreateICS
+    {
+        public static string CreateICSFile(DateTime start, DateTime end, string summary, string desc)
+        {
+            //some variables for demo purposes
+            DateTime DateStart = start;
+            DateTime DateEnd = end;
+            string Summary = summary;
+            string Location = "TEST LOC";
+            string Description = desc;
+
+            //create a new stringbuilder instance
+            StringBuilder sb = new StringBuilder();
+
+            //start the calendar item
+            sb.AppendLine("BEGIN:VCALENDAR");
+            sb.AppendLine("VERSION:2.0");
+            sb.AppendLine("PRODID:stackoverflow.com");
+            sb.AppendLine("CALSCALE:GREGORIAN");
+            sb.AppendLine("METHOD:PUBLISH");
+
+            //add the event
+            sb.AppendLine("BEGIN:VEVENT");
+
+            //with time zone specified
+            sb.AppendLine("DTSTAMP:" + DateTime.Now.ToUniversalTime().ToString("yyyyMMddTHHmmssZ"));
+            sb.AppendLine("UID:" + DateTime.Now.ToUniversalTime().ToString("yyMMddTHHmmssZ") + "superrandomstring-2281337" + new Random().Next(10000000));
+            sb.AppendLine("DTSTART;TZID=Europe/Tallinn:" + DateStart.ToString("yyyyMMddTHHmm00"));
+            sb.AppendLine("DTEND;TZID=Europe/Tallinn:" + DateEnd.ToString("yyyyMMddTHHmm00"));
+
+            sb.AppendLine("SUMMARY:" + Summary);
+            sb.AppendLine("LOCATION:" + Location);
+            sb.AppendLine("DESCRIPTION:" + Description);
+            sb.AppendLine("END:VEVENT");
+
+            //end calendar item
+            sb.AppendLine("END:VCALENDAR");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ WebApplication1/Models/Service.cs
@@ -0,0 +1,90 @@
+﻿using MimeKit;
+using System.Net.Mail;
+using System.Net;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using System.Text;
+using StartFromScratch;
+
+namespace WebApplication1.Models
+{
+    public class Service
+    {
+        /*private readonly ILogger<Service> logger;
+
+        public Service(ILogger<Service> logger)
+        {
+            this.logger = logger;
+        }*/
+
+        //System.Net.Mail.SmtpClient
+
+        public async void SendEmailDefault(string aeg, string Kuupaev, string email, DateTime start, DateTime end)
+        {
+            
+            try
+            {
+               
+                MailMessage message = new MailMessage();
+             
+                message.IsBodyHtml = true; 
+                message.From = new MailAddress("georgijblinov096@hotmail.com", "AutoService"); 
+                message.To.Add(email);
+                message.To.Add("georgijblinov096@hotmail.com");//адресат сообщения
+                message.Subject = "Сообщение от System.Net.Mail"; //тема сообщения
+                message.Body = "<div style=\"color: black;\">Tere!Äitah, et te kasutasite meie teenuseid, teie teenus on plaanitud " + Kuupaev +" в " + aeg + ". Soovime teile parimat päeva</div>";
+                //message.Attachments.Add(new Attachment("... путь к файлу ...")); 
+                using (FileStream fs = File.Create("gmail.ics"))
+                {
+                    byte[] text = new UTF8Encoding(true).GetBytes(CreateICS.CreateICSFile(start, end, "CarService purchase",
+                        ""));
+                    fs.Write(text, 0, text.Length);
+                }
+                message.Attachments.Add(new Attachment("gmail.ics"));
+                var client = new SmtpClient("smtp.office365.com", 587)
+                {
+                    //Credentials = new NetworkCredential("16285c1bbabdd1", "f9122b3e2925fd"),
+                    Credentials = new NetworkCredential("georgijblinov096@hotmail.com", "47304290263bl"),
+                    EnableSsl = true
+                };
+                client.Send(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+               // logger.LogError(e.GetBaseException().Message);
+            }
+        }
+
+        //MailKit.Net.Smtp.SmtpClient
+        /* public void SendEmailCustom()
+         {
+             try
+             {
+                 MimeMessage message = new MimeMessage();
+                 message.From.Add(new MailboxAddress("Моя компания", "admin@mycompany.com")); //отправитель сообщения
+                 message.To.Add(new MailboxAddress("mail@yandex.ru")); //адресат сообщения
+                 message.Subject = "Сообщение от MailKit"; //тема сообщения
+                 message.Body = new BodyBuilder() { HtmlBody = "<div style=\"color: green;\">Сообщение от MailKit</div>" }.ToMessageBody(); //тело сообщения (так же в формате HTML)
+
+                 using (MailKit.Net.Smtp.SmtpClient client = new MailKit.Net.Smtp.SmtpClient())
+                 {
+                     client.Connect("smtp.gmail.com", 587, true); //либо использум порт 465
+                     client.Authenticate("mail@gmail.com", "secret"); //логин-пароль от аккаунта
+                     client.Send(message);
+
+                     client.Disconnect(true);
+                     logger.LogInformation("Сообщение отправлено успешно!");
+                 }
+             }
+             catch (Exception e)
+             {
+                 logger.LogError(e.GetBaseException().Message);
+             }
+         }*/
+    }
+}
